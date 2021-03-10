601316c1d20da4abd4821a14e71842df906eece5
NowakArtur97 <nowart97@gmail.com>
2021-03-10 09:36:56 +0100

    Create Common Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment.secret';
 
 import { AppComponent } from './app.component';
 import { CardModule } from './card/card.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
-
-    CardModule,
-  ],
+  imports: [BrowserModule, CardModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/common/common.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment.secret';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
+})
+export class CommonModule {}
