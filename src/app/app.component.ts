276c0d4236ce81236be7c92269e95e3c46f7fb5f
NowakArtur97 <nowart97@gmail.com>
2021-03-11 17:17:51 +0100

    Dispatch Save Radicals Action in App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as RadicalActions from './radical/store/radical.actions';
+import AppStoreState from './store/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private store: Store<AppStoreState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(RadicalActions.saveRadicals());
+  }
 }
