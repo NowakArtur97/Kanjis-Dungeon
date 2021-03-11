f5ba414701d3b064d351cc53eec42ca19bda9f61
NowakArtur97 <nowart97@gmail.com>
2021-03-11 18:04:12 +0100

    Create basic Radical Card structure

--- src/app/card/card-radical/card-radical.component.ts
+++ src/app/card/card-radical/card-radical.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import Radical from 'src/app/radical/models/radical.model';
+import AppStoreState from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-card-radical',
   templateUrl: './card-radical.component.html',
   styleUrls: ['./card-radical.component.css'],
 })
-export class CardRadicalComponent {}
+export class CardRadicalComponent implements OnInit, OnDestroy {
+  private radicalSubscription$: Subscription;
+  currentRadical: Radical;
+
+  constructor(private store: Store<AppStoreState>) {}
+
+  ngOnInit(): void {
+    this.radicalSubscription$ = this.store
+      .select('radical')
+      .subscribe(({ radicals }) => (this.currentRadical = radicals[0]));
+  }
+
+  ngOnDestroy(): void {
+    this.radicalSubscription$?.unsubscribe();
+  }
+}
