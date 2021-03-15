6a9190b31da2d59164704efa3ad965e107927d42
NowakArtur97 <nowart97@gmail.com>
2021-03-15 08:21:59 +0100

    Change Kanji model

--- src/app/kanji/models/kanji.model.ts
+++ src/app/kanji/models/kanji.model.ts
@@ -1,7 +1,7 @@
 import Radical from 'src/app/radical/models/radical.model';
 
 export default interface Kanji extends Radical {
-  readonly onyomi?: string;
-  readonly kunyomi?: string;
-  readonly nanori?: string;
+  readonly onyomi?: string[];
+  readonly kunyomi?: string[];
+  readonly nanori?: string[];
 }
