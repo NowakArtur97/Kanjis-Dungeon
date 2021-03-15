7773c075975ed123fe6f04f62977665e51c8109e
NowakArtur97 <nowart97@gmail.com>
2021-03-15 08:14:07 +0100

    Fix issue with provider in Vocabulary Service

--- src/app/vocabulary/services/vocabulary.service.ts
+++ src/app/vocabulary/services/vocabulary.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import GenericFirebaseService from 'src/app/common/services/generic-firebase.service';
 
 import Word from '../models/word.model';
 
+@Injectable({ providedIn: 'root' })
 export default class VocabularyService extends GenericFirebaseService<Word> {
   constructor(protected httpClient: HttpClient) {
     super('vocabulary', httpClient);
