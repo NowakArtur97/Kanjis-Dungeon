a2be938d9859d5ab439207bfef65951480b0bb1c
NowakArtur97 <nowart97@gmail.com>
2021-03-19 07:42:54 +0100

    Show data based on Question type in Quiz Card Component

--- src/app/card/quiz-card/quiz-card.component.ts
+++ src/app/card/quiz-card/quiz-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
+import CharacterUtil from 'src/app/common/utils/character.util';
 import CommonValidators from 'src/app/common/validators/common.validator';
 import Radical from 'src/app/radical/models/radical.model';
 import AppStoreState from 'src/app/store/app.state';
@@ -38,14 +39,31 @@ export class QuizCardComponent implements OnInit, OnDestroy {
   private initForm(): void {
     let characters = '';
     let meanings = '';
+    let onyomi = [''];
+    let kunyomi = [''];
+    let nanori = [''];
+    let reading = '';
+
     if (this.currentCharacter) {
       characters = this.currentCharacter.characters;
       meanings = this.currentCharacter.meanings[0];
+
+      if (CharacterUtil.isKanji(this.currentCharacter)) {
+        onyomi = this.currentCharacter.onyomi;
+        kunyomi = this.currentCharacter.kunyomi;
+        nanori = this.currentCharacter.nanori;
+      } else if (CharacterUtil.isVocabulary(this.currentCharacter)) {
+        reading = this.currentCharacter.reading;
+      }
     }
 
     this.quizFormGroup = new FormGroup({
       characters: new FormControl(characters, [CommonValidators.notBlank]),
       meaning: new FormControl(meanings, [CommonValidators.notBlank]),
+      onyomi: new FormControl(onyomi[0], [CommonValidators.notBlank]),
+      kunyomi: new FormControl(kunyomi[0], [CommonValidators.notBlank]),
+      nanori: new FormControl(nanori[0], [CommonValidators.notBlank]),
+      reading: new FormControl(reading, [CommonValidators.notBlank]),
     });
   }
 
@@ -57,7 +75,28 @@ export class QuizCardComponent implements OnInit, OnDestroy {
     return this.quizFormGroup.get('meaning');
   }
 
+  get reading(): AbstractControl {
+    return this.quizFormGroup.get('reading');
+  }
+
+  get onyomi(): AbstractControl {
+    return this.quizFormGroup.get('onyomi');
+  }
+
+  get kunyomi(): AbstractControl {
+    return this.quizFormGroup.get('kunyomi');
+  }
+
+  get nanori(): AbstractControl {
+    return this.quizFormGroup.get('nanori');
+  }
+
   onValidateCard(): void {
     console.log('HELLO');
   }
+
+  isKanji = (): boolean => CharacterUtil.isKanji(this.currentCharacter);
+
+  isVocabulary = (): boolean =>
+    CharacterUtil.isVocabulary(this.currentCharacter);
 }
