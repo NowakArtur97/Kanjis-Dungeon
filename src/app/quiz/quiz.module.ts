322730100b6ad13b8d292006dd00ea81ccc40e22
NowakArtur97 <nowart97@gmail.com>
2021-03-19 12:15:41 +0100

    Create Quiz Effects with set next Question Effect

--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
+import QuizEffects from './store/quiz.effects';
 import { quizReducer } from './store/quiz.reducer';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forFeature('quiz', quizReducer)],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('quiz', quizReducer),
+    EffectsModule.forFeature([QuizEffects]),
+  ],
 })
 export class QuizModule {}
---
+++ src/app/quiz/store/quiz.effects.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { map, switchMap, withLatestFrom } from 'rxjs/operators';
+
+import AppStoreState from '../../store/app.state';
+import QuizService from '../services/quiz.service';
+import * as QuizActions from './quiz.actions';
+
+@Injectable()
+export default class QuizEffects {
+  constructor(
+    private actions$: Actions,
+    private store: Store<AppStoreState>,
+    private quizService: QuizService
+  ) {}
+
+  setNextQuestion$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(
+        QuizActions.setQuestions,
+        QuizActions.addAnswer,
+        QuizActions.addMistake
+      ),
+      withLatestFrom(this.store.select((state) => state.quiz.questions)),
+      switchMap(([action, questions]) =>
+        of(this.quizService.getNextQuestion(questions))
+      ),
+      map((nextQuestion) => QuizActions.setNextQuestion({ nextQuestion }))
+    )
+  );
+}
