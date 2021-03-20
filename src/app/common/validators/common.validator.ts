84157a778f022871b1459ef58cb3f034ce362f92
NowakArtur97 <nowart97@gmail.com>
2021-03-20 09:58:10 +0100

    Create equals method in Common Validator

--- src/app/common/validators/common.validator.ts
+++ src/app/common/validators/common.validator.ts
@@ -3,4 +3,9 @@ import { FormControl, ValidationErrors } from '@angular/forms';
 export default class CommonValidators {
   static notBlank = ({ value }: FormControl): ValidationErrors =>
     !value || value.length === 0 || !value.trim() ? { notBlank: true } : null;
+
+  static equals = (expected: any) => ({
+    value,
+  }: FormControl): ValidationErrors =>
+    expected !== value ? { equals: true } : null;
 }
--- src/app/radical/models/radical.model.ts
+++ src/app/radical/models/radical.model.ts
@@ -2,7 +2,7 @@ import CharacterType from 'src/app/common/enums/character-type.enum';
 
 export default interface Radical {
   readonly id: number;
-  characters: string;
+  readonly characters: string;
   readonly meanings: string[];
   readonly type: CharacterType;
 }
