23206c04df2f776ee3eb4fa8bd5a1b570d7e420e
NowakArtur97 <nowart97@gmail.com>
2021-03-25 09:52:49 +0100

    Create Radical Effects tests

---
+++ src/app/radical/store/tests/radical.effects.spec.ts
@@ -0,0 +1,145 @@
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { of, ReplaySubject } from 'rxjs';
+import CharacterType from 'src/app/common/enums/character-type.enum';
+import QuizService from 'src/app/quiz/services/quiz.service';
+import * as QuizActions from 'src/app/quiz/store/quiz.actions';
+import { QuizStoreState } from 'src/app/quiz/store/quiz.reducer';
+import AppStoreState from 'src/app/store/app.state';
+
+import Radical from '../../models/radical.model';
+import RADICALS from '../../radical.data';
+import RadicalService from '../../services/radical.service';
+import * as RadicalActions from '../radical.actions';
+import RadicalEffects from '../radical.effects';
+import { RadicalStoreState } from '../radical.reducer';
+
+const mockRadicals: Radical[] = [
+  {
+    id: 1,
+    characters: '一',
+    meanings: ['ground'],
+    type: CharacterType.RADICAL,
+  },
+  {
+    id: 2,
+    characters: '二',
+    meanings: ['two'],
+    type: CharacterType.RADICAL,
+  },
+];
+const radicalState: RadicalStoreState = {
+  radicals: mockRadicals,
+};
+const quizState: QuizStoreState = {
+  maxNumberOfQuestions: 12,
+  nextQuestion: null,
+  questions: [],
+  answers: [],
+  mistakes: [],
+};
+const mockState: Partial<AppStoreState> = {
+  radical: radicalState,
+  quiz: quizState,
+};
+
+describe('RadicalEffects', () => {
+  let radicalEffects: RadicalEffects;
+  let actions$: ReplaySubject<any>;
+  let store: Store<AppStoreState>;
+  let storeSpy: jasmine.Spy;
+  let radicalService: RadicalService;
+  let quizService: QuizService;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({})],
+      providers: [
+        RadicalEffects,
+        Store,
+        provideMockActions(() => actions$),
+        {
+          provide: RadicalService,
+          useValue: jasmine.createSpyObj('radicalService', ['save', 'getAll']),
+        },
+        {
+          provide: QuizService,
+          useValue: jasmine.createSpyObj('quizService', ['prepareQuestions']),
+        },
+      ],
+    })
+  );
+
+  beforeEach(() => {
+    radicalEffects = TestBed.inject(RadicalEffects);
+    store = TestBed.inject(Store);
+    radicalService = TestBed.inject(RadicalService);
+    quizService = TestBed.inject(QuizService);
+  });
+
+  describe('saveRadicals$', () => {
+    beforeEach(() => {
+      actions$ = new ReplaySubject(1);
+      actions$.next(RadicalActions.saveRadicals());
+      (radicalService.save as jasmine.Spy).and.returnValue(of(RADICALS));
+    });
+
+    it('should return a setRadicals action', () => {
+      radicalEffects.fetchRadicals$.subscribe((resultAction) => {
+        expect(resultAction).toEqual(
+          RadicalActions.setRadicals({ radicals: RADICALS })
+        );
+        expect(radicalService.save).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('fetchRadicals$', () => {
+    beforeEach(() => {
+      actions$ = new ReplaySubject(1);
+      actions$.next(RadicalActions.fetchRadicals());
+    });
+
+    it('when number of radicals on firebase is same or bigger than locally should return a setRadicals action', () => {
+      (radicalService.getAll as jasmine.Spy).and.returnValue(of(RADICALS));
+      radicalEffects.fetchRadicals$.subscribe((resultAction) => {
+        expect(resultAction).toEqual(
+          RadicalActions.setRadicals({ radicals: RADICALS })
+        );
+        expect(radicalService.getAll).toHaveBeenCalled();
+      });
+    });
+
+    it('when number of radicals on firebase is smaller than locally should return a saveRadicals action', () => {
+      (radicalService.getAll as jasmine.Spy).and.returnValue(of(mockRadicals));
+      radicalEffects.fetchRadicals$.subscribe((resultAction) => {
+        expect(resultAction).toEqual(RadicalActions.saveRadicals());
+        expect(radicalService.getAll).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('setQuestionsAboutRadicals$', () => {
+    beforeEach(() => {
+      actions$ = new ReplaySubject(1);
+      actions$.next(RadicalActions.setRadicals);
+      storeSpy = spyOn(store, 'select').and.callFake((selector) =>
+        of(mockState)
+      );
+      (quizService.prepareQuestions as jasmine.Spy).and.returnValue(
+        of(mockRadicals)
+      );
+    });
+
+    it('should return a setQuestions action', () => {
+      (radicalService.getAll as jasmine.Spy).and.returnValue(of(RADICALS));
+      radicalEffects.setQuestionsAboutRadicals$.subscribe((resultAction) => {
+        expect(resultAction).toEqual(
+          QuizActions.setQuestions({ questions: mockRadicals })
+        );
+        expect(quizService.prepareQuestions).toHaveBeenCalled();
+      });
+    });
+  });
+});
