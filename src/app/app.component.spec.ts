4e522280be0c114735d6281cc2c633c3bb51c4d2
NowakArtur97 <nowart97@gmail.com>
2021-03-25 11:23:31 +0100

    Create App Component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store, StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import * as KanjiActions from './kanji/store/kanji.actions';
+import * as RadicalActions from './radical/store/radical.actions';
+import AppStoreState from './store/app.state';
+import * as VocabularyActions from './vocabulary/store/vocabulary.actions';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: Store<AppStoreState>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [StoreModule.forRoot({}), RouterTestingModule],
+      providers: [Store],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    store = TestBed.inject(Store);
+
+    spyOn(store, 'dispatch');
+
+    fixture.detectChanges();
+    component.ngOnInit();
+  });
+
+  describe('when initialize component', () => {
+    it('should not dispatch saveRadicals, saveKanji and saveVocabulary actions', () => {
+      expect(store.dispatch).not.toHaveBeenCalledWith(
+        RadicalActions.saveRadicals()
+      );
+      expect(store.dispatch).not.toHaveBeenCalledWith(KanjiActions.saveKanji());
+      expect(store.dispatch).not.toHaveBeenCalledWith(
+        VocabularyActions.saveVocabulary()
+      );
+    });
   });
 });
