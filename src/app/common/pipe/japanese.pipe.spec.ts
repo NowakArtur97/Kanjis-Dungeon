226582860b50c2f1a3ba263e872bcea28a86629a
NowakArtur97 <nowart97@gmail.com>
2021-03-26 10:50:54 +0100

    Create tests for Japanese Pipe

--- src/app/common/pipe/japanese.pipe.spec.ts
+++ src/app/common/pipe/japanese.pipe.spec.ts
@@ -1,8 +1,43 @@
 import { JapanesePipe } from './japanese.pipe';
 
 describe('JapanesePipe', () => {
-  it('create an instance', () => {
-    const pipe = new JapanesePipe();
-    expect(pipe).toBeTruthy();
+  let pipe;
+
+  beforeEach(() => {
+    pipe = new JapanesePipe();
+  });
+
+  describe('with hiragana', () => {
+    it('should return text as hiragana', () => {
+      const hiragana = pipe.transform('suki');
+      expect(hiragana).toEqual('すき');
+    });
+
+    it('should return text as hiragana with double letter', () => {
+      const hiragana = pipe.transform('yatttsu');
+      expect(hiragana).toEqual('やっつ');
+    });
+  });
+
+  describe('with katakana', () => {
+    it('should return text as katakana', () => {
+      const katakana = pipe.transform('KAMERA');
+      expect(katakana).toEqual('カメラ');
+    });
+
+    it('should return text as katakana with double letter', () => {
+      const katakana = pipe.transform('ROKKKU');
+      expect(katakana).toEqual('ロック');
+    });
+  });
+
+  it('with incorrect data should return text without transforming', () => {
+    const text = pipe.transform('XBcD');
+    expect(text).toEqual('XBcD');
+  });
+
+  it('with empty text should return empty string', () => {
+    const text = pipe.transform('');
+    expect(text).toEqual('');
   });
 });
