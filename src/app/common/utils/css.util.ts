0352f522c35708eac68cc6b714c0bbc11da73849
NowakArtur97 <nowart97@gmail.com>
2021-04-17 12:53:40 +0200

    Setup Sprite based on resolution and values from component

--- src/app/common/utils/css.util.ts
+++ src/app/common/utils/css.util.ts
@@ -6,4 +6,10 @@ export default class CssUtil {
       CssUtil.MAIN_CARD_COLOR_CSS_VARIABLE,
       color
     );
+
+  static changeCSSVariable = (variableName: string, value: any): void =>
+    document.documentElement.style.setProperty(variableName, value);
+
+  static getCSSVariable = (variableName: string): string =>
+    getComputedStyle(document.body).getPropertyValue(variableName).slice(1);
 }
--- src/app/game/character/character-sprite/character-sprite.component.ts
+++ src/app/game/character/character-sprite/character-sprite.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import CssUtil from 'src/app/common/utils/css.util';
 
 @Component({
   selector: 'app-character-sprite',
   templateUrl: './character-sprite.component.html',
   styleUrls: ['./character-sprite.component.css'],
 })
-export class CharacterSpriteComponent implements OnInit {
+export class CharacterSpriteComponent implements OnInit, AfterViewInit {
+  private characterImage = 'example-character';
+  private numberOfFrames = 4;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const characterImage: string =
+      CssUtil.getCSSVariable('--sprite-src') +
+      this.characterImage +
+      '_' +
+      CssUtil.getCSSVariable('--sprite-size-multiplier') +
+      CssUtil.getCSSVariable('--sprite-extension');
+    CssUtil.changeCSSVariable('--sprite-image', `url(${characterImage})`);
+    CssUtil.changeCSSVariable('--sprite-frames', this.numberOfFrames);
+  }
 }
