573a81d12ed68e055ba5e727b169950a5f8cbb7c
NowakArtur97 <nowart97@gmail.com>
2021-04-28 17:16:18 +0200

    Add styles and structure for Character Statuses Component

--- src/app/game/character/character-statuses/character-statuses.component.ts
+++ src/app/game/character/character-statuses/character-statuses.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import CharacterStatus from '../models/character-status.model';
+
 @Component({
   selector: 'app-character-statuses',
   templateUrl: './character-statuses.component.html',
   styleUrls: ['./character-statuses.component.css'],
 })
 export class CharacterStatusesComponent implements OnInit {
+  private STATUSES_PATH = '../../../../assets/statuses';
+  private STATUS_IMAGE_EXTENSION = '.png';
+
+  statuses: CharacterStatus[];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // TODO: CharacterStatusesComponent: Get by Input
+    this.statuses = [
+      {
+        spriteSheet: `${this.STATUSES_PATH}/heart${this.STATUS_IMAGE_EXTENSION}`,
+        remainingNumberOfActiveRounds: 2,
+      },
+      {
+        spriteSheet: `${this.STATUSES_PATH}/book${this.STATUS_IMAGE_EXTENSION}`,
+        remainingNumberOfActiveRounds: 3,
+      },
+    ];
+  }
 }
---
+++ src/app/game/character/models/character-status.model.ts
@@ -0,0 +1,4 @@
+export default interface CharacterStatus {
+  spriteSheet: string;
+  remainingNumberOfActiveRounds: number;
+}
