6686db97809607c8833b0db17ea842de1150d291
NowakArtur97 <nowart97@gmail.com>
2021-09-18 11:20:53 +0200

    Use data files in Japanese Pipe

--- src/app/japanese/common/pipes/japanese.pipe.ts
+++ src/app/japanese/common/pipes/japanese.pipe.ts
@@ -1,5 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import HIRAGANA from '../hiragana.data';
+import KATAKANA from '../katakana.data';
+
 @Pipe({
   name: 'japanese',
   pure: false,
@@ -10,252 +13,8 @@ export class JapanesePipe implements PipeTransform {
   private readonly ALL_LETTERS_REG_EXP = /[a-zA-Z]/g;
   private readonly DOUBLED_LETTER_HIRAGANA = 'っ';
   private readonly DOUBLED_LETTER_KATAKANA = 'ッ';
-  private hiraganaMappings: Map<string, string> = new Map([
-    ['ka', 'か'],
-    ['ki', 'き'],
-    ['ku', 'く'],
-    ['ke', 'け'],
-    ['ko', 'こ'],
-    ['kya', 'きゃ'],
-    ['kyu', 'きゅ'],
-    ['kyo', 'きょ'],
-    ['ga', 'が'],
-    ['gi', 'ぎ'],
-    ['gu', 'ぐ'],
-    ['ge', 'げ'],
-    ['go', 'ご'],
-    ['gya', 'ぎゃ'],
-    ['gyu', 'ぎゅ'],
-    ['gyo', 'ぎょ'],
 
-    ['ta', 'た'],
-    ['chi', 'ち'],
-    ['tsu', 'つ'],
-    ['te', 'て'],
-    ['to', 'と'],
-    ['cha', 'ちゃ'],
-    ['chu', 'ちゅ'],
-    ['cho', 'ちょ'],
-    ['da', 'だ'],
-    ['dji', 'ぢ'],
-    ['dzu', 'づ'],
-    ['de', 'で'],
-    ['do', 'ど'],
-    ['dya', 'ぢゃ'],
-    ['dyu', 'ぢゅ'],
-    ['dyo', 'ぢょ'],
-
-    ['sa', 'さ'],
-    ['shi', 'し'],
-    ['su', 'す'],
-    ['se', 'せ'],
-    ['so', 'そ'],
-    ['sha', 'しゃ'],
-    ['shu', 'しゅ'],
-    ['sho', 'しょ'],
-    ['za', 'ざ'],
-    ['ji', 'じ'],
-    ['zu', 'ず'],
-    ['ze', 'ぜ'],
-    ['zo', 'ぞ'],
-    ['jya', 'じゃ'],
-    ['jyu', 'じゅ'],
-    ['jyo', 'じょ'],
-
-    ['na', 'な'],
-    ['ni', 'に'],
-    ['nu', 'ぬ'],
-    ['ne', 'ね'],
-    ['no', 'の'],
-    ['nya', 'にゃ'],
-    ['nyu', 'にゅ'],
-    ['nyo', 'にょ'],
-
-    ['ha', 'は'],
-    ['hi', 'ひ'],
-    ['fu', 'ふ'],
-    ['he', 'へ'],
-    ['ho', 'ほ'],
-    ['hya', 'ひゃ'],
-    ['hyu', 'ひゅ'],
-    ['hyo', 'ひょ'],
-    ['ba', 'ば'],
-    ['bi', 'び'],
-    ['bu', 'ぶ'],
-    ['be', 'べ'],
-    ['bo', 'ぼ'],
-    ['bya', 'びゃ'],
-    ['byu', 'びゅ'],
-    ['byo', 'びょ'],
-    ['pa', 'ぱ'],
-    ['pi', 'ぴ'],
-    ['pu', 'ぷ'],
-    ['pe', 'ぺ'],
-    ['po', 'ぽ'],
-    ['pya', 'ぴゃ'],
-    ['pyu', 'ぴゅ'],
-    ['pyo', 'ぴょ'],
-
-    ['ma', 'ま'],
-    ['mi', 'み'],
-    ['mu', 'む'],
-    ['me', 'め'],
-    ['mo', 'も'],
-    ['mya', 'みゃ'],
-    ['myu', 'みゅ'],
-    ['myo', 'みょ'],
-
-    ['ra', 'ら'],
-    ['ri', 'り'],
-    ['ru', 'る'],
-    ['re', 'れ'],
-    ['ro', 'ろ'],
-    ['rya', 'りゃ'],
-    ['ryu', 'りゅ'],
-    ['ryo', 'りょ'],
-
-    ['ya', 'や'],
-    ['yu', 'ゆ'],
-    ['yo', 'よ'],
-
-    ['wa', 'わ'],
-    ['wi', 'ゐ'],
-    ['we', 'ゑ'],
-    ['wo', 'を'],
-
-    ['a', 'あ'],
-    ['i', 'い'],
-    ['u', 'う'],
-    ['e', 'え'],
-    ['o', 'お'],
-
-    ['nn', 'ん'],
-  ]);
-
-  private katakanaMappings: Map<string, string> = new Map([
-    ['KA', 'カ'],
-    ['KI', 'キ'],
-    ['KU', 'ク'],
-    ['KE', 'ケ'],
-    ['KO', 'コ'],
-    ['KYA', 'キャ'],
-    ['KYU', 'キュ'],
-    ['KYO', 'キョ'],
-    ['GA', 'ガ'],
-    ['GI', 'ギ'],
-    ['GU', 'グ'],
-    ['GE', 'ゲ'],
-    ['GO', 'ゴ'],
-    ['GYA', 'ギャ'],
-    ['GYU', 'ギュ'],
-    ['GYO', 'ギョ'],
-
-    ['TA', 'タ'],
-    ['CHI', 'チ'],
-    ['TSU', 'ツ'],
-    ['TE', 'テ'],
-    ['TO', 'ト'],
-    ['CHA', 'チャ'],
-    ['CHU', 'チュ'],
-    ['CHO', 'チョ'],
-    ['DA', 'ダ'],
-    ['JI', 'ヂ'],
-    ['DZU', 'ヅ'],
-    ['DE', 'デ'],
-    ['DO', 'ド'],
-    ['DYA', 'ヂャ'],
-    ['DYU', 'ヂュ'],
-    ['DYO', 'ヂョ'],
-
-    ['SA', 'サ'],
-    ['SHI', 'シ'],
-    ['SU', 'ス'],
-    ['SE', 'セ'],
-    ['SO', 'ソ'],
-    ['SHA', 'シャ'],
-    ['SHU', 'シュ'],
-    ['SHO', 'ショ'],
-    ['ZA', 'ザ'],
-    ['JI', 'ジ'],
-    ['ZU', 'ズ'],
-    ['ZE', 'ゼ'],
-    ['ZO', 'ゾ'],
-    ['JYA', 'ジゃ'],
-    ['JYU', 'ジゅ'],
-    ['JYO', 'ジょ'],
-
-    ['NA', 'ナ'],
-    ['NI', 'ニ'],
-    ['NU', 'ヌ'],
-    ['NE', 'ネ'],
-    ['NO', 'ノ'],
-    ['NYA', 'ニャ'],
-    ['NYU', 'ニュ'],
-    ['NYO', 'ニョ'],
-
-    ['HA', 'ハ'],
-    ['HI', 'ヒ'],
-    ['FU', 'フ'],
-    ['HE', 'ヘ'],
-    ['HO', 'ホ'],
-    ['HYA', 'ヒャ'],
-    ['HYU', 'ヒュ'],
-    ['HYO', 'ヒョ'],
-    ['BA', 'バ'],
-    ['BI', 'ビ'],
-    ['BU', 'ブ'],
-    ['BE', 'ベ'],
-    ['BO', 'ボ'],
-    ['BYA', 'ビャ'],
-    ['BYU', 'ビュ'],
-    ['BYO', 'ビョ'],
-    ['PA', 'パ'],
-    ['PI', 'ピ'],
-    ['PU', 'プ'],
-    ['PE', 'ペ'],
-    ['PO', 'ポ'],
-    ['PYA', 'ピャ'],
-    ['PYU', 'ピュ'],
-    ['PYO', 'ピョ'],
-
-    ['MA', 'マ'],
-    ['MI', 'ミ'],
-    ['MU', 'ム'],
-    ['ME', 'メ'],
-    ['MO', 'モ'],
-    ['MYA', 'ミャ'],
-    ['MYU', 'ミュ'],
-    ['MYO', 'ミョ'],
-
-    ['RA', 'ラ'],
-    ['RI', 'リ'],
-    ['RU', 'ル'],
-    ['RE', 'レ'],
-    ['RO', 'ロ'],
-    ['RYA', 'リャ'],
-    ['RYU', 'リュ'],
-    ['RYO', 'リョ'],
-
-    ['YA', 'ヤ'],
-    ['YU', 'ユ'],
-    ['YO', 'ヨ'],
-
-    ['WA', 'ワ'],
-    ['WI', 'ヰ'],
-    ['WE', 'ヱ'],
-    ['WO', 'ヲ'],
-
-    ['A', 'ア'],
-    ['I', 'イ'],
-    ['U', 'ウ'],
-    ['E', 'エ'],
-    ['O', 'オ'],
-
-    ['NN', 'ン'],
-  ]);
-  private allMappings = new Map(
-    [...this.hiraganaMappings].concat([...this.katakanaMappings])
-  );
+  private allMappings = [...HIRAGANA, ...KATAKANA];
 
   transform(value: string): string {
     if (!value) {
@@ -296,9 +55,9 @@ export class JapanesePipe implements PipeTransform {
 
   private convertToJapanese(word: string): string {
     while (this.containsAnyLetters(word)) {
-      this.allMappings.forEach((value, key) => {
-        if (word.includes(key)) {
-          word = word.replace(new RegExp(key, 'g'), value);
+      this.allMappings.forEach((letter) => {
+        if (word.includes(letter.record)) {
+          word = word.replace(new RegExp(letter.record, 'g'), letter.sign);
         }
       });
     }
