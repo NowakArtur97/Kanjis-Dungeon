ae32a217ad3d92c92206310edb583ef9471b90a3
NowakArtur97 <nowart97@gmail.com>
2021-09-27 19:35:50 +0200

    Allow switching between Hiragana and Katakana

--- src/app/japanese/common/components/japanese-alphabet/japanese-alphabet.component.ts
+++ src/app/japanese/common/components/japanese-alphabet/japanese-alphabet.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import HIRAGANA from '../../hiragana.data';
+import KATAKANA from '../../katakana.data';
 import Letter from '../../models/letter.model';
 
 @Component({
@@ -10,10 +11,16 @@ import Letter from '../../models/letter.model';
 })
 export class JapaneseAlphabetComponent implements OnInit {
   alphabet: Letter[];
+  isHiraganaActive = true;
 
   constructor() {}
 
   ngOnInit(): void {
     this.alphabet = HIRAGANA;
+  }
+
+  onChangeAlphabet(): void {
+    this.isHiraganaActive = !this.isHiraganaActive;
+    this.alphabet = this.isHiraganaActive ? HIRAGANA : KATAKANA;
   }
 }
